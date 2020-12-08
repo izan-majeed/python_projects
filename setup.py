83d3d62bf9e6bd21b852ceff1f79ecf48b79e876
izan <izan-majeed>
2020-12-08 13:27:12 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'keyword-helper',
+    version = '0.0.1',
+    description = "A simple python keyword helper which depends on 'tkinter' package",
+    py_modules = ['keyword_helper',],
+    package_dir = {'':'src'}
+)
